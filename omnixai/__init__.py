d6304f9ed2e3a6cd26c9168735ee1342ca22993f
yangwenzhuo08 <wenzhuo.yang@salesforce.com>
2022-05-17 17:51:54 +0800

    A library for explainable AI

--- omnixai/__init__.py
+++ omnixai/__init__.py
@@ -4,3 +4,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    dist = get_distribution("omnixai")
+except DistributionNotFound:
+    __version__ = "Please install OmniXAI with setup.py"
+else:
+    __version__ = dist.version
