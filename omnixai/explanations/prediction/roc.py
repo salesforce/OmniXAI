f06cec1f86aff6a526a95ce30324d66fcaae4450
ywz <yangwenzhuo08@gmail.com>
2022-07-04 15:07:34 +0800

    ROC curves for classification

--- omnixai/explanations/prediction/roc.py
+++ omnixai/explanations/prediction/roc.py
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
+"""
+The ROC curves.
+"""
 from typing import Dict
 from ..base import ExplanationBase, DashFigure
 
