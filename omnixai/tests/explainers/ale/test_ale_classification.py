445f42b1bc88c70dacb33b6b875b81a4cc1633a3
yangwenzhuo08 <wenzhuo.yang@salesforce.com>
2022-08-11 14:39:49 +0800

    Add save and load functions

--- omnixai/tests/explainers/ale/test_ale_classification.py
+++ omnixai/tests/explainers/ale/test_ale_classification.py
@@ -14,12 +14,25 @@ from omnixai.tests.explainers.tasks import TabularClassification
 
 class TestALE(unittest.TestCase):
 
-    def test(self):
+    def test_1(self):
         set_random_seed()
         base_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
         task = TabularClassification(base_folder).train_adult(num_training_samples=2000)
         predict_function = lambda z: task.model.predict_proba(task.transform.transform(z))
         explainer = ALE(training_data=task.train_data, predict_function=predict_function)
+        explanations = explainer.explain()
+        pprint.pprint(explanations.get_explanations())
+
+    def test_2(self):
+        set_random_seed()
+        task = TabularClassification().train_iris()
+        predict_function = lambda z: task.model.predict_proba(task.transform.transform(z))
+        explainer = ALE(training_data=task.train_data, predict_function=predict_function)
+
+        base_folder = os.path.dirname(os.path.abspath(__file__))
+        directory = f"{base_folder}/../../datasets/tmp"
+        explainer.save(directory=directory)
+        explainer = ALE.load(directory=directory)
         explanations = explainer.explain()
         pprint.pprint(explanations.get_explanations())
 
