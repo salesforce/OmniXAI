b00ff516b88f193300274258c14033a926f27238
ywz <yangwenzhuo08@gmail.com>
2022-08-22 14:55:52 +0800

    Update __init__.py

--- omnixai/explainers/vision_language/__init__.py
+++ omnixai/explainers/vision_language/__init__.py
@@ -5,8 +5,9 @@
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
 from .specific.gradcam import GradCAM
-
+from .specific.ig import IntegratedGradient
 
 __all__ = [
     "GradCAM",
+    "IntegratedGradient"
 ]
