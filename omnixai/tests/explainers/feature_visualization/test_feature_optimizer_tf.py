ccb918197a66c3102b91bd496325d4883d9009b9
ywz <yangwenzhuo08@gmail.com>
2022-08-29 22:01:11 +0800

    Feature visualization

---
+++ omnixai/tests/explainers/feature_visualization/test_feature_optimizer_tf.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2022 salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+#
+import os
+import unittest
+import tensorflow as tf
+
+
+class TestFeatureOptimizer(unittest.TestCase):
+
+    def test_build_model(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
