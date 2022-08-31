8a4da88ea5fa06d52996b1fa54886b4056459970
ywz <yangwenzhuo08@gmail.com>
2022-08-31 16:32:45 +0800

    Feature visualization

--- omnixai/explainers/vision/specific/feature_visualization/pytorch/preprocess.py
+++ omnixai/explainers/vision/specific/feature_visualization/pytorch/preprocess.py
@@ -14,7 +14,7 @@ class RandomBlur(TransformBase):
     Blurs image with randomly chosen Gaussian blur.
     """
 
-    def __init__(self, kernel_size, sigma=(0.1, 2.0)):
+    def __init__(self, kernel_size, sigma=(0.8, 1.2)):
         super().__init__()
         self.transformer = torchvision.transforms.GaussianBlur(
             kernel_size, sigma=sigma)
