6330c63cdff09603a065e6151778e3e2328c50d5
ywz <yangwenzhuo08@gmail.com>
2022-09-05 09:45:28 +0800

    Feature visualization

--- omnixai/tests/explainers/feature_visualization/test_feature_optimizer_torch.py
+++ omnixai/tests/explainers/feature_visualization/test_feature_optimizer_torch.py
@@ -5,6 +5,7 @@
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
 import unittest
+import numpy as np
 import torch.nn as nn
 from omnixai.explainers.vision.specific.feature_visualization.pytorch.optimizer import \
     Objective, FeatureOptimizer
@@ -34,15 +35,29 @@ class TestFeatureOptimizer(unittest.TestCase):
         self.model = Model()
 
     def test_layer(self):
-        objective = Objective(
-            layer=self.model.layers[4]
-        )
+        objectives = [
+            Objective(
+                layer=self.model.layers[4]
+            ),
+            Objective(
+                layer=self.model.layers[4],
+                channel_indices=0
+            ),
+            Objective(
+                layer=self.model.layers[4],
+                neuron_indices=[0, 1]
+            ),
+            Objective(
+                layer=self.model.layers[4],
+                direction_vectors=np.random.random((16, 26, 26))
+            ),
+        ]
         optimizer = FeatureOptimizer(
             model=self.model,
-            objectives=objective
+            objectives=objectives
         )
         optimizer.optimize(
-            num_iterations=10,
+            num_iterations=2,
             image_shape=(64, 64)
         )
 
