0f23cf289790937c9a28bebd15eba8ae5bae114a
ywz <yangwenzhuo08@gmail.com>
2022-09-12 22:33:22 +0800

    Score-CAM for vision models

--- omnixai/explainers/vision/specific/scorecam/tf/scorecam.py
+++ omnixai/explainers/vision/specific/scorecam/tf/scorecam.py
@@ -6,6 +6,7 @@
 #
 import numpy as np
 from typing import Callable
+from tqdm import trange
 
 from omnixai.data.image import Image
 from omnixai.preprocessing.image import Resize
@@ -42,3 +43,30 @@ class ScoreCAM:
     def explain(self, X: Image, y=None, **kwargs):
         assert min(X.shape[1:3]) > 4, f"The image size ({X.shape[1]}, {X.shape[2]}) is too small."
         explanations = PixelImportance(self.mode, use_heatmap=True)
+        inputs = self.preprocess(X) if self.preprocess is not None else X.to_numpy()
+        model = tf.keras.Model(self.model.input, [self.target_layer.output, self.model.output])
+
+        if self.mode == "classification":
+            if y is not None:
+                if type(y) == int:
+                    y = [y for _ in range(len(X))]
+                else:
+                    assert len(X) == len(y), (
+                        f"Parameter ``y`` is a {type(y)}, the length of y "
+                        f"should be the same as the number of images in X."
+                    )
+        else:
+            y = None
+
+        inputs = tf.convert_to_tensor(inputs)
+        activations, predictions = model(inputs)
+        if self.mode == "classification" and y is None:
+            y = tf.argmax(predictions, axis=-1).numpy()
+
+        print(activations.shape)
+        print(predictions.shape)
+
+        weights = []
+        for i in trange(activations.shape[-1]):
+            saliency = activations[..., i:i + 1]
+            pass
