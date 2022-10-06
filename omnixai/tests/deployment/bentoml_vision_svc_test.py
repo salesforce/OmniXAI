91b5cb13bcf963f95e198322c04e6631ca546bb1
ywz <yangwenzhuo08@gmail.com>
2022-10-06 21:06:51 +0800

    Deployment on BentoML

--- omnixai/tests/deployment/bentoml_vision_svc_test.py
+++ omnixai/tests/deployment/bentoml_vision_svc_test.py
@@ -17,7 +17,8 @@ class TestService(unittest.TestCase):
 
     def setUp(self) -> None:
         directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../datasets/images/")
-        self.img = Resize((256, 256)).transform(Image(PilImage.open(directory + "dog_cat.png").convert("RGB")))
+        img = Resize((256, 256)).transform(Image(PilImage.open(directory + "dog_cat.png").convert("RGB")))
+        self.test_instance = img.to_numpy()[0]
 
     def test(self):
         svc = init_service(
@@ -28,9 +29,9 @@ class TestService(unittest.TestCase):
         for runner in svc.runners:
             runner.init_local()
 
-        predictions = svc.apis["predict"].func(self.img)
+        predictions = svc.apis["predict"].func(self.test_instance)
         print(predictions)
-        local_explanations = svc.apis["explain"].func(self.img, {})
+        local_explanations = svc.apis["explain"].func(self.test_instance, {})
         print(local_explanations)
 
         import json
