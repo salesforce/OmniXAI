100d16b3cfdf3e46bf250c0f67fed3707e92ace1
yangwenz <yangwenzhuo08@gmail.com>
2023-02-03 14:39:10 +0800

    Add what-if explanation

--- omnixai/visualization/callbacks/whatif_exp.py
+++ omnixai/visualization/callbacks/whatif_exp.py
@@ -9,17 +9,64 @@ from ..pages.whatif_exp import create_result_column
 @callback(
     Output("whatif-explanation-state", "data"),
     [
-        Input("select-instance-whatif", "value")
+        Input("select-instance-whatif", "value"),
+        Input("first-instance-set-btn", "n_clicks"),
+        Input("first-instance-reset-btn", "n_clicks"),
+        Input("second-instance-set-btn", "n_clicks"),
+        Input("second-instance-reset-btn", "n_clicks"),
     ],
-    State("whatif-explanation-state", "data")
+    [
+        State("first-instance-feature-name", "value"),
+        State("first-instance-feature-value", "value"),
+        State("second-instance-feature-name", "value"),
+        State("second-instance-feature-value", "value"),
+        State("whatif-explanation-state", "data")
+    ]
 )
-def change_parameters(instance, data):
-    params = json.loads(data) if data is not None else {}
+def change_parameters(
+        instance,
+        first_set_click,
+        first_reset_click,
+        second_set_click,
+        second_reset_click,
+        first_feature_name,
+        first_feature_value,
+        second_feature_name,
+        second_feature_value,
+        state_data
+):
+    params = json.loads(state_data) if state_data is not None else {}
     ctx = dash.callback_context
     if ctx.triggered:
         prop_id = ctx.triggered[0]["prop_id"].split(".")[0]
         if prop_id == "select-instance-whatif":
             params["display_instance"] = int(instance)
+            board.whatif_state.set_display_instance(int(instance))
+
+        elif prop_id == "first-instance-set-btn":
+            if first_feature_name and first_feature_value:
+                index = board.whatif_state.get_display_instance()
+                example = board.whatif_state.get_instance("instances-a", index).to_pd()
+                example[first_feature_name] = first_feature_value
+                board.whatif_state.set_instance("instances-a", index, example.iloc[0])
+
+        elif prop_id == "first-instance-reset-btn":
+            index = board.whatif_state.get_display_instance()
+            example = board.whatif_state.instances.iloc(index).to_pd()
+            board.whatif_state.set_instance("instances-a", index, example.iloc[0])
+
+        elif prop_id == "second-instance-set-btn":
+            if second_feature_name and second_feature_value:
+                index = board.whatif_state.get_display_instance()
+                example = board.whatif_state.get_instance("instances-b", index).to_pd()
+                example[second_feature_name] = second_feature_value
+                board.whatif_state.set_instance("instances-b", index, example.iloc[0])
+
+        elif prop_id == "second-instance-reset-btn":
+            index = board.whatif_state.get_display_instance()
+            example = board.whatif_state.instances.iloc(index).to_pd()
+            board.whatif_state.set_instance("instances-b", index, example.iloc[0])
+
     return json.dumps(params)
 
 
